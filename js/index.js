749b234ce8fd78c0bfef8b74b757fde964dc64d4
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2020-09-05 11:22:52 +0800

    109.09.05

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 let data = {
+    product:{
+        now_page:1,
+        movex:"",
+        info:[
+           { product_img_src:"../images/product_example1.svg"},
+           { product_img_src:"../images/product_example2.svg"},
+           { product_img_src:"../images/product_example3.svg"},
+           { product_img_src:"../images/product_example4.svg"},
+           { product_img_src:"../images/product_example5.svg"},
+        ]
+    },
     exhibition: {
         now_page: 1,
         movex: "transform: translateX(0);",
@@ -18,6 +29,23 @@ let vm = new Vue({
     el: "#app",
     data: data,
     computed: {
+        product_loop(){
+            let count = this.product.info.length
+            let new_product = this.product.info
+            console.log(count);
+            if(count > 3){
+                new_product.push(
+                    {product_img_src : this.product.info[0].product_img_src},
+                    {product_img_src : this.product.info[1].product_img_src},
+                    {product_img_src : this.product.info[2].product_img_src},
+                )
+                for(i=0 ; i<3 ; i++){
+                    new_product.unshift(this.product.info[count - 1])
+                }
+               
+            }
+            return new_product;
+        },
         exhibition_loop() {
             let count = this.exhibition.info.length
             let new_exhibition = this.exhibition.info
@@ -35,21 +63,26 @@ let vm = new Vue({
         }
     },
     methods: {
-        prev() {
+        prev(str) {
             if (!this.btn_action) return;
-            if(this.exhibition.info.length == 1) return //如果只有一筆資料不顯示按鈕，這行可拿掉
-            console.log("改變前" + this.exhibition.now_page);
-            this.exhibition.now_page -= 1;
-            console.log("改變後" + this.exhibition.now_page);
-            if (this.exhibition.now_page <= 0) {
-
-                this.btnHandler();
-                this.exhibition.movex = "transform: translateX(-" + this.exhibition.now_page * 100 + "%);";
-                this.exhibition.now_page = this.exhibition.info.length - 2;
-                let loop_first = setTimeout(this.loopHandler, 300);
-            } else {
-                this.exhibition.movex = "transform: translateX(-" + this.exhibition.now_page * 100 + "%)";
+            if(str === "exhibition"){
+                if(this.exhibition.info.length == 1) return //如果只有一筆資料不顯示按鈕，這行可拿掉
+                console.log("改變前" + this.exhibition.now_page);
+                this.exhibition.now_page -= 1;
+                console.log("改變後" + this.exhibition.now_page);
+                if (this.exhibition.now_page <= 0) {
+    
+                    this.btnHandler();
+                    this.exhibition.movex = "transform: translateX(-" + this.exhibition.now_page * 100 + "%);";
+                    this.exhibition.now_page = this.exhibition.info.length - 2;
+                    let loop_first = setTimeout(this.loopHandler, 300);
+                } else {
+                    this.exhibition.movex = "transform: translateX(-" + this.exhibition.now_page * 100 + "%)";
+                }
+            }else if(str === "product"){
+                console.log(this.product.info.length);
             }
+            
         },
         next() {
             if (!this.btn_action) return;
