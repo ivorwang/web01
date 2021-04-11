54c4905941bd96bce14d3ed3fec742d1c01fa369
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-04-11 09:14:26 +0800

    110.04.11 download版面差不多惹，功能尚未完成

---
+++ new_layout/js/download.js
@@ -0,0 +1,37 @@
+var download = document.querySelector('.download');
+var download_box = document.querySelector('.download_box');
+var download_item = document.querySelectorAll('.download_item');
+// 左右多家15間距
+var download_pic_width = document.querySelector('.download_item_pic').clientWidth + 30;
+var download_pic_height = document.querySelector('.download_item_pic').clientHeight + 20;
+// console.log(download_pic_height);
+var download_tab = document.querySelector('.download_tab');
+// 寬度縮放偵測
+window.onresize = function () {
+    sizeHnadler()
+}
+
+
+
+function sizeHnadler(){
+    let download_box_height = download.clientHeight - download_tab.clientHeight;
+    
+    if (window.screen.width > 767) {
+        let newItem_w = parseInt(download_box.clientWidth / download_pic_width);
+        download_item[0].style.width = "calc(100% / 4 )";
+        for (var i = 0; i < download_item.length; i++) {
+            download_item[i].style.width = "calc(100% /" + newItem_w + ")"
+        }
+    } else {
+        for (var i = 0; i < download_item.length; i++) {
+            download_item[i].style.width = ""
+        }
+    }
+
+    console.log(parseInt(download_box_height / download_pic_height));
+
+    // parseInt(download_box.clientHeight - download_tab.clientHeight);
+}
+
+
+sizeHnadler()
