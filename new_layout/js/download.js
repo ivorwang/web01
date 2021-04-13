db03d64a8e42ec6fca81b910cc585819f38495e6
yji35j42012 <yji35j42012@gmail.com>
2021-04-13 17:41:27 +0800

    download js 修正

--- new_layout/js/download.js
+++ new_layout/js/download.js
@@ -3,7 +3,7 @@ var download_box = document.querySelector('.download_box');
 var download_item = document.querySelectorAll('.download_item');
 // 左右多家15間距
 var download_pic_width = document.querySelector('.download_item_pic').clientWidth + 30;
-var download_pic_height = document.querySelector('.download_item_pic').clientHeight + 20;
+// var download_pic_height = document.querySelector('.download_item_pic').clientHeight + 20;
 // console.log(download_pic_height);
 var download_tab = document.querySelector('.download_tab');
 // 寬度縮放偵測
@@ -11,10 +11,8 @@ window.onresize = function () {
     sizeHnadler()
 }
 
-
-
 function sizeHnadler(){
-    let download_box_height = download.clientHeight - download_tab.clientHeight;
+    // let download_box_height = download.clientHeight - download_tab.clientHeight;
     
     if (window.screen.width > 767) {
         let newItem_w = parseInt(download_box.clientWidth / download_pic_width);
@@ -28,7 +26,7 @@ function sizeHnadler(){
         }
     }
 
-    console.log(parseInt(download_box_height / download_pic_height));
+    // console.log(parseInt(download_box_height / download_pic_height));
 
     // parseInt(download_box.clientHeight - download_tab.clientHeight);
 }
