c09324fc940cebf32f5dbd74e4d0891f47567d07
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-04-28 08:30:44 +0800

    about 加入框架

---
+++ new_layout/components/about/about.vue
@@ -0,0 +1,19 @@
+<style scoped></style>
+
+<template src="./components/about/about.html"></template>
+
+
+<script>
+module.exports = {
+	data() {
+		return {
+			tabNum: 0,
+		};
+	},
+	methods: {
+		tabHandler() {
+			console.log(this.tabNum);
+		},
+	},
+};
+</script>
