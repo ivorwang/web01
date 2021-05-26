f59c60a4dca51ce28a70bfe304ac0487e17721d4
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2021-05-26 17:21:11 +0800

    新增 character gif comics video 畫面 以及 圖片路徑整理

--- new_layout/components/download/download.vue
+++ new_layout/components/download/download.vue
@@ -5,43 +5,48 @@
 
 <script>
 module.exports = {
-	data() {
-		return {
-			tabNum: 0,
-		};
-	},
-	mounted(){
-		window.onresize = () => {
-			return (() => {
-				// this.$route
-				if(this.$route.path == "/download") {
-					this.sizeHnadler()
-				}
-			})()
-		}
-	},
-	methods:{
-		sizeHnadler(){
-			// var download = document.querySelector('.download');
-			var download_box = document.querySelector('.download_box');
-			var download_item = document.querySelectorAll('.download_item');
-			// 左右多家15間距
-			var download_pic_width = document.querySelector('.download_item_pic').clientWidth + 30;
-			// var download_pic_height = document.querySelector('.download_item_pic').clientHeight + 20;
-			// console.log(download_pic_height);
-			// var download_tab = document.querySelector('.download_tab');
-			if (window.screen.width > 767) {
-				let newItem_w = parseInt(download_box.clientWidth / download_pic_width);
-				download_item[0].style.width = "calc(100% / 4 )";
-				for (var i = 0; i < download_item.length; i++) {
-					download_item[i].style.width = "calc(100% /" + newItem_w + ")";
-				}
-			} else {
-				for (var i = 0; i < download_item.length; i++) {
-					download_item[i].style.width = "";
-				}
-			}
-		}
-	}
+    data() {
+        return {
+            tabNum: 0,
+        };
+    },
+
+    mounted() {
+        window.onresize = () => {
+            return (() => {
+                // this.$route
+                if (this.$route.path == "/download") {
+                    this.sizeHnadler();
+                }
+            })();
+        };
+    },
+    methods: {
+        sizeHnadler() {
+            // var download = document.querySelector('.download');
+            var download_box = document.querySelector(".download_box");
+            var download_item = document.querySelectorAll(".download_item");
+            // 左右多家15間距
+            var download_pic_width =
+                document.querySelector(".download_item_pic").clientWidth + 30;
+            // var download_pic_height = document.querySelector('.download_item_pic').clientHeight + 20;
+            // console.log(download_pic_height);
+            // var download_tab = document.querySelector('.download_tab');
+            if (window.screen.width > 767) {
+                let newItem_w = parseInt(
+                    download_box.clientWidth / download_pic_width
+                );
+                download_item[0].style.width = "calc(100% / 4 )";
+                for (var i = 0; i < download_item.length; i++) {
+                    download_item[i].style.width =
+                        "calc(100% /" + newItem_w + ")";
+                }
+            } else {
+                for (var i = 0; i < download_item.length; i++) {
+                    download_item[i].style.width = "";
+                }
+            }
+        },
+    },
 };
 </script>
